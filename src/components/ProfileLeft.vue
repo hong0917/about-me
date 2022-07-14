80aeeb27a330cacb1025ade25c1556a77aa31d33
hong0917 <dbrlshddl@gmail.com>
2022-07-14 13:55:58 +0900

    header 및 main_menu 추가

--- src/components/ProfileLeft.vue
+++ src/components/ProfileLeft.vue
@@ -8,20 +8,22 @@
         <div class="header_title">
           <h2>Lesser_Hong</h2>
           <h4>Backend Developer</h4>
-          <ul class="social-links">
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
         </div>
         <div class="header_btn">
-          <button>Contact Me</button>
+          <button class="call_btn" @click="copyEmail()">Contact Me</button>
         </div>
       </div>
     </transition>
 
     <transition name="fade" v-show="showView">
       <div class="main_menu">
+        <ul class="social-links">
+          <li><a href=""><img class="icon" src="../assets/img/facebook.png" alt=""></a></li>
+          <li><a href=""><img class="icon" src="../assets/img/premium-icon-instagram-3991634.png" alt=""></a></li>
+          <li><a href=""><img class="icon" src="../assets/img/free-icon-github-5968866.png" alt=""></a></li>
+          <li><a href=""><img class="icon" src="../assets/img/back.png" alt=""></a></li>
+          <li><a href=""><img class="icon" src="../assets/img/next.png" alt=""></a></li>
+        </ul>
       </div>
     </transition>
   </div>
@@ -41,6 +43,10 @@ export default {
   methods:{
     changeView(){
         this.showView = !this.showView
+    },
+
+    copyEmail(){
+      alert("이메일 주소가 복사되었습니다.")
     }
   }
 }
--- src/components/ProfileRight.vue
+++ src/components/ProfileRight.vue
@@ -1,11 +1,29 @@
 <template>
   <div class="profile_right">
+    <transition name="fade">
+      <div class="site_content" v-show="showView">
+
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ProfileRight'
+  name: 'ProfileRight',
+  data(){
+    return{
+      showView:false
+    }
+  },
+  mounted(){
+    setTimeout(this.changeView,500)
+  },
+  methods:{
+    changeView(){
+        this.showView = !this.showView
+    },
+  }
 }
 </script>
 
