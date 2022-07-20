504aebeb5bd76fa1e5b6b7013964d28e1265f21d
hong0917 <dbrlshddl@gmail.com>
2022-07-20 13:11:09 +0900

    github icon 변경

--- src/view/ProfileLeft.vue
+++ src/view/ProfileLeft.vue
@@ -17,7 +17,7 @@
         <ul class="social-links">
           <li><a href=""><img class="icon" src="../assets/img/facebook.png" alt="facebook_icon"></a></li>
           <li><a href=""><img class="icon" src="../assets/img/premium-icon-instagram-3991634.png" alt="instagram_icon"></a></li>
-          <li><a href=""><img class="icon" src="../assets/img/free-icon-github-5968866.png" alt="github_icon"></a></li>
+          <li><a href=""><img class="icon" src="../assets/img/free-icon-github-sign-25657.png" alt="github_icon"></a></li>
           <li><a href=""><img class="icon" src="../assets/img/back.png" alt="back_icon"></a></li>
           <li><a href=""><img class="icon" src="../assets/img/next.png" alt="next_icon"></a></li>
         </ul>
