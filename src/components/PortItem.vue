8c5601bb9f35eac09dd2b732cab64bc70a72ccb9
hong <dbrlshddl@gmail.com>
2022-07-26 23:29:46 +0900

    item 세부창 작업

--- src/components/PortItem.vue
+++ src/components/PortItem.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="portItem">
+    
   </div>
 </template>
 
--- src/components/Portpolio.vue
+++ src/components/Portpolio.vue
@@ -6,73 +6,73 @@
 
     <div class="list">
       <ul>
-        <li @click="showItem()">
+        <li @click="changeItem()">
           <div class="item">
             <h3 class="item_title">이투스 - 다양한 강의를 제공하는 플렛폼 </h3>
             <span class="item_date">2021.04.14 ~ 2021.11.25</span>
           </div>
         </li>
-        <li @click="showItem()">
+        <li @click="changeItem()">
           <div class="item">
             <h3 class="item_title">나이키 - 나이키 관련 판매 사이트 </h3>
             <span class="item_date">2021.04.14 ~ 2021.11.25</span>
           </div>
         </li>
-        <li @click="showItem()">
+        <li @click="changeItem()">
           <div class="item">
             <h3 class="item_title">테스트 - 여차저차하는 그런저런 플렛폼 개발 </h3>
             <span class="item_date">2021.04.14 ~ 2021.11.25</span>
           </div>
         </li>
-        <li @click="showItem()">
+        <li @click="changeItem()">
           <div class="item">
             <h3 class="item_title">하나은행 - 상담사들을 위한 챗봇 유지보수 </h3>
             <span class="item_date">2021.04.14 ~ 2021.11.25</span>
           </div>
         </li>
-         <li @click="showItem()">
+        <li @click="changeItem()">
           <div class="item">
             <h3 class="item_title">이투스 - 다양한 강의를 제공하는 플렛폼 </h3>
             <span class="item_date">2021.04.14 ~ 2021.11.25</span>
           </div>
         </li>
-        <li @click="showItem()">
+        <li @click="changeItem()">
           <div class="item">
             <h3 class="item_title">나이키 - 나이키 관련 판매 사이트 </h3>
             <span class="item_date">2021.04.14 ~ 2021.11.25</span>
           </div>
         </li>
-        <li @click="showItem()">
+        <li @click="changeItem()">
           <div class="item">
             <h3 class="item_title">테스트 - 여차저차하는 그런저런 플렛폼 개발 </h3>
             <span class="item_date">2021.04.14 ~ 2021.11.25</span>
           </div>
         </li>
-        <li @click="showItem()">
+        <li @click="changeItem()">
           <div class="item">
             <h3 class="item_title">하나은행 - 상담사들을 위한 챗봇 유지보수 </h3>
             <span class="item_date">2021.04.14 ~ 2021.11.25</span>
           </div>
         </li>
-         <li @click="showItem()">
+        <li @click="changeItem()">
           <div class="item">
             <h3 class="item_title">이투스 - 다양한 강의를 제공하는 플렛폼 </h3>
             <span class="item_date">2021.04.14 ~ 2021.11.25</span>
           </div>
         </li>
-        <li @click="showItem()">
+        <li @click="changeItem()">
           <div class="item">
             <h3 class="item_title">나이키 - 나이키 관련 판매 사이트 </h3>
             <span class="item_date">2021.04.14 ~ 2021.11.25</span>
           </div>
         </li>
-        <li @click="showItem()">
+        <li @click="changeItem()">
           <div class="item">
             <h3 class="item_title">테스트 - 여차저차하는 그런저런 플렛폼 개발 </h3>
             <span class="item_date">2021.04.14 ~ 2021.11.25</span>
           </div>
         </li>
-        <li @click="showItem()">
+        <li @click="changeItem()">
           <div class="item">
             <h3 class="item_title">하나은행 - 상담사들을 위한 챗봇 유지보수 </h3>
             <span class="item_date">2021.04.14 ~ 2021.11.25</span>
@@ -80,17 +80,30 @@
         </li>
       </ul>
     </div>
+    <transition name="fade">
+    <PortItem v-if="showItem"></PortItem>
+    </transition>
   </div>
 </template>
 
 <script>
+import PortItem from '../components/PortItem.vue'
+
 export default {
   name: 'portpolio',
   data(){
     return{
-      
+      showItem:false
     }
   },
+  components:{
+    PortItem
+  },
+  methods:{
+    changeItem(){
+      this.showItem = !this.showItem
+    }
+  }
 }
 </script>
 
